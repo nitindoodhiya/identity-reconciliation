1a5d06acba60f8f843ded785e34a021e07ceb5c0
nitin <nitindoodhiya@gmail.com>
2023-06-10 18:43:01 +0530

    Handle same email and different number or vice versa and update the linkedId and precedence

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { userModel } = require('../models/user');
 const { ObjectId } = require('mongodb');
 
 
-String.prototype.hashCode = function() {
+String.prototype.hashCode = function () {
   var hash = 0;
   if (this.length == 0) {
     return hash;
@@ -41,30 +41,35 @@ const formatOutput = (array) => {
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/identify', function(req, res, next) {
+router.get('/identify', function (req, res, next) {
   res.render('identify', { title: 'Express' });
 });
-
-router.post('/identify', async function(req, res, next) {
+async function getContact({ email, phoneNumber }) {
+  const emailUsers = await userModel.find({ email });
+  const phoneNumbers = [];
+  emailUsers.forEach((user) => {
+    phoneNumbers.push(user.phoneNumber);
+  });
+  phoneNumbers.push(phoneNumber);
+  const users = await userModel.find({ phoneNumber: { $in: phoneNumbers } });
+  return users;
+}
+router.post('/identify', async function (req, res, next) {
   const { email, phoneNumber } = req.body;
   let user = await userModel.findOne({ email, phoneNumber });
   if (user) {
-    const emailUsers = await userModel.find({email});
-    const phoneNumbers = [];
-    emailUsers.forEach((user) => {
-      phoneNumbers.push(user.phoneNumber);
-    });
-    const users = await userModel.find({phoneNumber: { $in: phoneNumbers }});
-    res.send({ contact: formatOutput(users), message: 'users exists' });
+    const contact = await getContact({ email, phoneNumber });
+    res.send({ contact: formatOutput(contact) });
   }
   else {
-    const existingEntry = await userModel.find({$or: [{ email }, {phoneNumber}], linkPrecedence: 'primary' });
+    const existingEntry = await userModel.find({ $or: [{ email }, { phoneNumber }], linkPrecedence: 'primary' });
     let existingPhoneNumberPrimary = null;
     let existingEmailPrimary = null;
+
     existingEntry.forEach((entry) => {
       if (entry.email === email) {
         existingEmailPrimary = entry;
@@ -73,20 +78,34 @@ router.post('/identify', async function(req, res, next) {
         existingPhoneNumberPrimary = entry;
       }
     });
+    let filter = {};
+    let update = {};
+    let create = true;
+    if (existingEmailPrimary && existingPhoneNumberPrimary) {
+      filter = { phoneNumber: existingPhoneNumberPrimary.phoneNumber };
+      update = { $set: { linkPrecedence: 'secondary', linkedId: existingEmailPrimary.id, updatedAt: new Date() } };
+      create = false;
+    }
     const primaryUser = existingEmailPrimary || existingPhoneNumberPrimary;
-    const objecId = new ObjectId();
-    user = new userModel({
-      id: objecId.toString().hashCode(),
-      phoneNumber,
-      email,
-      linkedId: primaryUser ? primaryUser.id : null,
-      linkPrecedence: primaryUser ? 'secondary' : 'primary',  
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      deletedAt: null,
-    });
-    await user.save();
-    res.send(user);
+    await userModel.updateMany(filter, update);
+
+    if (create) {
+      const objectId = new ObjectId();
+      const id = objectId.toString().hashCode();
+      user = new userModel({
+        id,
+        phoneNumber,
+        email,
+        linkPrecedence: primaryUser ? 'secondary' : 'primary',
+        linkedId: primaryUser ? primaryUser.id : null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      });
+      await user.save();
+    }
+    const contact = await getContact({ email, phoneNumber });
+    res.send({ contact: formatOutput(contact) });
   }
 });
 
